f2aa6ac27f0be8c424aac8075316a03065e7f511
Silvan Mosberger <contact@infinisil.com>
2021-08-17 01:43:53 +0200

    Lowercase usernames

--- module.nix
+++ module.nix
@@ -6,19 +6,21 @@ let
   login = pkgs.writeShellScript "login" ''
     set -euo pipefail
 
+    user=''${SSH_USER,,}
+
     # Get the primary group of the user
-    if ! group=$(id -gn "$SSH_USER" 2>/dev/null); then
+    if ! group=$(id -gn "$user" 2>/dev/null); then
       # This fails if the user doesn't exist, so create it
-      useradd -b /home/participants -m -g participants "$SSH_USER"
-      chmod g+rX /home/participants/"$SSH_USER"
+      useradd -b /home/participants -m -g participants "$user"
+      chmod g+rX /home/participants/"$user"
     elif [[ ! "$group" == participants ]]; then
       # If the user exists, but its primary group is not participants, we don't allow users to log into it
-      echo "User \"$SSH_USER\" cannot be used to participate with"
+      echo "User \"$user\" cannot be used to participate with"
       exit 1
     fi
 
     # Log into the user, which should now exist and be a participating user
-    exec machinectl --quiet shell "$SSH_USER"@
+    exec machinectl --quiet shell "$user"@
   '';
 in {
   options.services.ssh-participation = {
